8283f0c3a211875e99f980d55fe99c0f4206dadd
ElRafaelGod <rafaelmartinezpage@gmail.com>
2021-12-16 12:11:00 +0100

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2"
+       @click=" $router.push({ name: 'entry', params: {id: 10} }) ">
+
+      <div class="entry-title d-flex">
+          <span class="text-success fs-5 fw-bold">16</span>
+          <span class="mx-1 fs-5">Diciembre</span>
+          <span class="mx-2 fw-light">2021, Jueves</span>
+      </div>
+
+      <div class="entry-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quis dolorem delectus ex a adipisci cum vero maiores, eos illo eius eum nam modi exercitationem. Accusamus quo aliquid suscipit molestias.
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .entry-container{
+        border-bottom: 1px solid #2c3e50;
+        transition: 0.2s all ease-in;
+
+        &:hover{
+            background-color: lighten($color: grey, $amount: 45);
+            transition: 0.2s all ease-in;
+        }
+        
+        .entry-description{
+            font-size: 12px;
+        }
+    }
+
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,27 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime,
+            date: new Date().toDateString(),
+            text: 'MMMMnidajnfdkasjfdguhaidgfygahsuidcbvsuhvjaguyhiv ugayndfsbvbgyahnusvfb. WTYVFBUHCFJBAGSUHAFAS,IHAVJHDAISDGYIA hjadnkshbjduafdjal',
+            picture: null,
+        },
+        {
+            id: new Date().getTime + 1000,
+            date: new Date().toDateString(),
+            text: '001010010000011000110010 00100 0001010100011001 0010100110, 00100100110010110 001001001001000001001011110101 00100100110010101000101',
+            picture: null,
+        },
+        {
+            id: new Date().getTime + 2000,
+            date: new Date().toDateString(),
+            text: 'lALALAMDUBAJNLKGUHDJJAAJSJDJLASDJLASDKADSJ. ODAOID, IUGAHEIJFDEAM, OIEIOEFJANKNLDKIHLASJNDKANDAJHFANWHNFDHKANFDKAFSJ',
+            picture: null,
+        },
+        
+    ]
+})
+
+
